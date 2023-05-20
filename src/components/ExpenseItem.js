a2f944e7b067f1cc39e6ac7fc738aad8f401acd9
Rajasagar <rasto.rajaa@gmail.com>
2023-05-20 23:49:18 +0530

    Create new componets ExpenseDate and add css to it

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,19 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate'
 function ExpenseItem(props) {
             // const expensesDate = new Date(2023, 5, 21);
             // const expenseTitle = "car Insurance";
             // const expenseAmount = 2987.78;
             // we are creating different varaible to store the month day year. and use as componets in below
-            const month = props.date.toLocaleString('en-US', {month: 'long'});
-            const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-            const year = props.date.getFullYear();
+            // const month = props.date.toLocaleString('en-US', {month: 'long'});
+            // const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+            // const year = props.date.getFullYear();
            
 
     return (
       <div className="expense-item">
         {/* above variable use as month day year to store below div components  */}
-        <div>
-            <div>{month}</div>
-            <div>{year}</div>
-            <div>{day}</div>
-        </div>
+        <ExpenseDate date={props.date} />
         {/* <div>{props.date.toISOString()}</div> */}
         <div className="expense-item__description">
           <h2>{props.title}</h2>
