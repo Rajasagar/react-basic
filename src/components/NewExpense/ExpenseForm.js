b05c41fcc1cb2d6a6753cb456710bd66ad776db9
Rajasagar <rasto.rajaa@gmail.com>
2023-05-22 23:53:01 +0530

    combine the all useState in one single useState and update the function for all 3.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,20 +3,40 @@ import './ExpenseForm.css'
 
 const ExpenseForm =()=>{
     //document.getElementById('').addEventListener('click', (event) =>{})
-    const [enteredTitle, setEnteredTitle]= useState('')
+
+    const [userInput, setUserInput] = useState({
+        enteredTitle:'',
+        enteredAmount:'',
+        enteredDate: ''
+    }); // uses all the useState in one places
+
+   // const [enteredTitle, setEnteredTitle]= useState('')
     const titlechangeHandler = (event)=>{
         // console.log(event.target.value);
-        setEnteredTitle(event.target.value)
-    }
-    const [enteredAmount, setEnteredAmount] = useState('')
+        //setEnteredTitle(event.target.value)
+        //use other way to store the value when we combing the useState as one
+        setUserInput({
+            ...userInput,
+            enteredTitle:event.target.value
+        });
+    };
+    //const [enteredAmount, setEnteredAmount] = useState('')
     const amountChangeHandler = (event)=>{
-        setEnteredAmount(event.target.value)
-    }
-    const [enteredDate, setEnteredDate] =  useState('')
+        //setEnteredAmount(event.target.value)
+        setUserInput({
+            ...userInput,
+            enteredAmount:event.target.value
+        });
+    };
+    //const [enteredDate, setEnteredDate] =  useState('')
     const dateChangeHandler = (event)=>{
-        setEnteredDate(event.target.value)
+        //setEnteredDate(event.target.value)
+        setUserInput({
+            ...userInput,
+            enteredDate:event.target.value
+        });
 
-    }
+    };
     return (
       <form>
         <div className="new-expense__controls">
